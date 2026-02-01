172a21caf83a2a1988f8466d998919ffb086277e
kimcul76 <kimcul76@naver.com>
2026-02-01 23:47:31 +0900

    주석추가

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine($"이름 : {person.Name} / 나이 : {person.Age}");
 
 Person person2 = new Person { PersonId = 2, Name = "손흥민", Age = 30 };
 Console.WriteLine($"이름 : {person2.Name} / 나이 : {person2.Age}");
+
+/**/
