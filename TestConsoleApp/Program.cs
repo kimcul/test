c2a16ee4696cebb6d8e1c9c6d30a363ec5e3b2b3
kimcul76 <kimcul76@naver.com>
2026-02-01 22:28:43 +0900

    git ignore file add, Person 클래스 추가

--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Person person = new Person{ PersonId = 1, Name = "홍길동", Age = 20 };
+Console.WriteLine($"이름 : {person.Name} / 나이 : {person.Age}");
